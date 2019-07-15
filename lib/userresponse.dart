6379a47b847362915ea21c67cc8bdda4b509a142
Agnes Kavata Musyoka <kavatamusyoka24@gmail.com>
2019-07-15 11:55:11 +0300

    introduce response class

---
+++ lib/userresponse.dart
@@ -0,0 +1,17 @@
+import 'package:dio_app/models/user.dart';
+
+class UserResponse {
+  final List<User> results;
+  final String error;
+
+  UserResponse(this.results, this.error);
+
+  UserResponse.fromJson(Map<String, dynamic> json)
+      : results =
+  (json["results"] as List).map((i) => new User.fromJson(i)).toList(),
+        error = "";
+
+  UserResponse.withError(String errorValue)
+      : results = List(),
+        error = errorValue;
+}
